9865bb7c77238aa2fa3ff679992527da942dbac7
Spence Patrick <spence_patrick@intuit.com>
2021-04-29 20:05:57 -0600

    try some stuff

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import os
 import http.server
 import socketserver
-
+from bs4 import BeautifulSoup
 from http import HTTPStatus
-
+import requests
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+
+        url = 'https://www.google.com/'
+        r = requests.get(url)
+        r.text
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hello! you requested %s' % (r.text)
         self.wfile.write(msg.encode())
 
 
